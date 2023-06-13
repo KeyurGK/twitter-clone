cfafa5409b48555b950dca21ce7920e1521864cf
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-06-13 22:55:35 +0530

    installed materialUI dependancies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 function App() {
   return (
     <div className="App">
       <h1>Twitter Clone</h1>
+      <TwitterIcon />
+      <TwitterIcon />
     </div>
   );
 }
