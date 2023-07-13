554744d429ca9132afdceb1c9fe7aae459c41dcc
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-07-13 19:29:01 +0530

    config appwrite

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,1 +1,10 @@
-import { Client } from "appwrite";
+import { Client, Account } from "appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("64aff9a7dad1eef19290");
+
+export const account = new Account(Client);
+export default client;
